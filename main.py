12efa7e8f72773c6a86d9a1c05adfd8ee9897bb3
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2023-10-23 10:56:08 -0400

    Re-factored get & set to be O(1) instead of linear.

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ class DynamicArray:
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.data = [None] * capacity
+        self.size = 0
 
     def get(self, i: int) -> int:
-        index = self.findTrueIndexAtIndex(i)
-        value = self.data[index]
-        return value
+        return self.data[i]
 
     def findTrueIndexAtIndex(self, i):
 
@@ -22,22 +21,18 @@ class DynamicArray:
             index += 1
 
     def set(self, i: int, n: int) -> None:
-        index = self.findTrueIndexAtIndex(i)
-        self.data[index] = n
+        self.data[i] = n
 
     def pushback(self, n: int) -> None:
         if self.getSize() == self.getCapacity():
             self.resize()
 
-        reverseIndex = self.findFirstElementReverse(None, 0)
+        self.data[self.size] = n
+        self.size += 1
 
-        self.data[reverseIndex] = n
-        data_to_shift = self.data[reverseIndex + 1:]
-        self.data = self.data[:reverseIndex]
-        self.data += data_to_shift
-        self.data.append(n)
 
     def findFirstElementReverse(self, element=None, comparator=0):
+
         def comparison_function(value):
             return value == element if comparator == 0 else value != element
 
@@ -48,31 +43,25 @@ class DynamicArray:
             reverseIndex = len(self.data) - 1 - index
             value = self.data[reverseIndex]
             if reverseIndex < 0 or comparison_function(self.data[reverseIndex]):
-                return reverseIndex  # This will return -1 if no element is found, adjust as needed.
+                return reverseIndex
             index += 1
 
-        return reverseIndex
-
     def popback(self) -> int:
-        reverseIndex = self.findFirstElementReverse(None, 1)
-        value = self.data[reverseIndex]
-        self.data[reverseIndex] = None
+        self.size -= 1
+        value = self.data[self.size]
+        self.data[self.size] = None
         return value
 
     def resize(self) -> None:
         temp = self.data
-        self.data = [None] * len(temp) * 2
+        self.capacity = len(temp) * 2
+        self.data = [None] * self.capacity
 
         for index, element in enumerate(temp):
             self.data[index] = element
 
     def getSize(self) -> int:
-        size = 0
-        for elem in self.data:
-            if elem is not None:
-                size += 1
-
-        return size
+        return self.size
 
     def getCapacity(self) -> int:
-        return len(self.data)
+        return self.capacity
